2e81bd573a51983024f3e7ce4e2f10b9f262daab
jared <jaredspecjr@gmail.com>
2018-06-18 11:54:12 -0700

    add 2 new compnents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule
