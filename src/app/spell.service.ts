90eda8bf18c5a78fc9ba74713508d87b491860dc
Kristen Chellis and Kayl Eubanks <kayl@>
2018-06-19 09:48:49 -0700

    Write functions for getting spells by id and by class

--- src/app/spell.service.ts
+++ src/app/spell.service.ts
@@ -15,6 +15,44 @@ export class SpellService {
     request.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
           spells = JSON.parse(this.response);
+        } else if (this.readyState === 4 && this.status != 200) {
+          spells = "No spells";
+        }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    return spells;
+  }
+
+  getSpellById(spellId: number) {
+    let request = new XMLHttpRequest();
+    let url = "https://dnd-spell-organizer.herokuapp.com/classes/"+classId+"/spells";
+    let spells;
+    request.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          spells = JSON.parse(this.response);
+        } else if (this.readyState === 4 && this.status != 200) {
+          spells = "This class has no spells.";
+        }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    return spells;
+  }
+
+  getSpellsByClass(classId: number) {
+    let request = new XMLHttpRequest();
+    let url = "https://dnd-spell-organizer.herokuapp.com/spells";
+    let spells;
+    request.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          spells = JSON.parse(this.response);
+        } else if (this.readyState === 4 && this.status != 200) {
+          spells = "No spells";
         }
     }
 
