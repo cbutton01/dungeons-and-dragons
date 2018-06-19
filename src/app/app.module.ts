4d8916a56c1c285cd5ec2afb6db81382aa6a6e12
cb <csbutton01@hotmail.com>
2018-06-19 08:10:30 -0700

    create components for specific elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PlayerComponent } from './player/player.component';
+import { RoutingComponent } from './routing/routing.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PlayerComponent
+    PlayerComponent,
+    RoutingComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
