a129cc1d13080b8b2e0ce44b3387bef8dc5e94e4
Kristen Chellis and Kayl Eubanks <kayl@>
2018-06-19 11:26:31 -0700

    remove doubled property declarations from models

--- src/app/models/character.model.ts
+++ src/app/models/character.model.ts
@@ -1,10 +1,3 @@
 export class Character {
-  name: string;
-  race: string;
-  classId: number;
-  castingLevel: number;
-  spellCount: number;
-  preparedSpells: string;
-  id: number;
   constructor(public name: string, public race: string, public classId: number, public castingLevel: number, public spellCount: number, public preparedSpells: string, public id: number) {}
 }
